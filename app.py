502356190e3e6c663ff3330edd010d3f168c03f3
john-kliem <jkliem@gmu.edu>
2025-05-02 21:32:27 -0400

    Updates

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 import json
 import os
 import subprocess
@@ -6,6 +6,10 @@ import subprocess
 app = Flask(__name__)
 CONFIG_PATH = os.path.join(os.path.dirname(__file__), 'wp_config.json')
 SUBMISSIONS_PATH = os.path.join(os.path.dirname(__file__), 'submissions')
+
+USERNAME = 'pi'
+PASSWORD = 'raspberry'
+
 
 def load_config():
     try:
@@ -15,6 +19,7 @@ def load_config():
         print(f"Error loading config: {e}")
         return {"teams": {"blue": [], "red": []}}
 
+
 def list_zip_files():
     try:
         # print("ZipFiles: ", SUBMISSIONS_PATH)
@@ -23,11 +28,13 @@ def list_zip_files():
         print(f"Error listing submissions: {e}")
         return []
 
+
 @app.route('/', methods=['GET'])
 def index():
     config = load_config()
     zip_files = list_zip_files()
     return render_template('index.html', teams=config['teams'], zip_files=zip_files)
+
 
 @app.route('/submit', methods=['POST'])
 def submit():
@@ -37,14 +44,70 @@ def submit():
         handle_submission(team, zip_file)
     return redirect(url_for('index'))
 
+
+@app.route('/agent_action', methods=['POST'])
+def agent_action():
+    config = load_config()
+    team = request.form.get('team')
+    boat_id = request.form.get('boat_id')
+    boat_name = request.form.get('boat_name')
+    boat_ip = request.form.get('boat_ip')
+    boat_port = request.form.get('boat_port')
+    action = request.form.get('action')
+    print(f"Team: {team}, Boat ID: {boat_id}, Boat Name: {boat_name}, Boat IP: {boat_ip}, Boat Port: {boat_port}, Action: {action}")
+    command = ''
+
+    if config['shore_ip'] == 'localhost':    
+        command = f"python submission_runner.py --entry_name=test.zip --sim --color={team} --boat_id={boat_id} --boat_name={boat_name} --timewarp=4 --shore_ip={config['shore_ip']} --boat_ip={boat_ip} --boat_port={boat_port}"#f"sshpass -p '{PASSWORD}' rsync -av --progress ./submissions/{filename} {USERNAME}@{ip}:{dest_path}"
+    else:
+        command = f"python submission_runner.py --entry_name=test.zip --color={team} --boat_id={boat_id} --boat_name={boat_name} --timewarp=1 --shore_ip={config['shore_ip']} --boat_ip={boat_ip} --boat_port={boat_port}"
+        command = f"sshpass -p {PASSWORD} ssh {USERNAME}@{boat_ip} " + command
+    subprocess.Popen(['gnome-terminal', '--', 'bash', '-c', f"{command}; exec bash"])
+    return redirect(url_for('index'))
+
+
 def handle_submission(team, zip_file):
     print(f"Received submission for {team} team: {zip_file}")
     config = load_config()
-    config['team']
-    for bot in config['teams']:
-        command = "sshpass -p 'raspberry' rsync -av --progress ./submissions/{zip_file} {username}@{config['teams']['ip']}:/entries/"
+    file_path = ""
+    if config['shore_ip'] == 'localhost': #We are running in sim copy team files to local dirs
+        #Ensure the folder can only ever have one entry 
+        subprocess.run(["rm", "-rf", f"~/USV_Deploy_UI/{team}_entry"])
+        subprocess.run(["mkdir", "-p", f"~/USV_Deploy_UI/{team}_entry"])
+
+
+        if 'red' == team:
+            file_path = '~/USV_Deploy_UI/red_entry/test.zip'
+        else:
+            file_path = '~/USV_Deploy_UI/blue_entry/test.zip'
+    if config['shore_ip'] == 'localhost':
+        command = "cp ~/USV_Deploy_UI/submissions/"+zip_file+" "+file_path+" ; exec bash"
         subprocess.Popen(['gnome-terminal','--', 'bash', '-c', command]) #Launch MacOS/Linux machines 
-    # Do something with the file here: unzip, copy, run, etc.
+    else:
+        for bot in config['teams'][team]:
+            command = "sshpass -p "+PASSWORD+" rsync -av --progress ./submissions/{zip_file} {USERNAME}@{bot['ip']}:~/entries/ ; exec bash"
+            subprocess.Popen(['gnome-terminal','--', 'bash', '-c', command]) #Launch MacOS/Linux machines 
+
+
+@app.route('/get_rsync_command', methods=['POST'])
+def get_rsync_command():
+    config = load_config()
+    data = request.json
+    team = data['team']
+    boat_id = data['boat_id']
+    boat_name = data['boat_name']
+    boat_ip = data['boat_ip']
+    boat_port = data['boat_port']
+    action = data['target']
+    path = "~/USV_Deploy_UI/"
+    path += "blue_entry/" if team == 'blue' else "red_entry/"
+    
+    if config['shore_ip'] == 'localhost':    
+        command = f"python submission_runner.py --sim --boat_id={boat_id} --boat_name={boat_name} --timewarp=4 --shore_ip={config['shore_ip']} --boat_ip={boat_ip} --boat_port={boat_port}"#f"sshpass -p '{PASSWORD}' rsync -av --progress ./submissions/{filename} {USERNAME}@{ip}:{dest_path}"
+    else:
+        command = f"python submission_runner.py --boat_id={boat_id} --boat_name={boat_name} --timewarp=1 --shore_ip={config['shore_ip']} --boat_ip={boat_ip} --boat_port={boat_port}"
+        #command = f"sshpass -p '{PASSWORD}' "
+    return jsonify({'command': command})
 
 
 if __name__ == '__main__':
---
+++ submission_runner.py
@@ -0,0 +1,57 @@
+import docker
+import subprocess
+import time
+import sys
+import socket
+import random
+import argparse
+import signal
+import sys
+
+entry_docker = None
+def signal_handler(sig, frame):
+    if entry_docker:
+        print('Closing Docker')
+        #TODO Find command to locate logs and copy all log files
+        entry_docker.kill()
+    sys.exit(0)
+
+
+def run_game(entry_path, sim, color, boat_id, boat_name, timewarp, shore_ip, boat_ip, boat_port):
+    global entry_docker
+    client = docker.from_env()
+    entry_docker = client.containers.run('jkliem/wp25:latest', command="sleep infinity", detach=True, network_mode='host')
+    subprocess.run(['docker','cp', entry_path, str(entry_docker.short_id)+':/home/moos/workingdir/test.zip']) #Copy file into new docker
+    if sim:
+        arguments = '--sim'
+    else:
+        arguments = ' '
+    arguments += '--color ' + color
+    arguments += ' --boat_id ' + boat_id
+    arguments += ' --boat_name' + boat_name
+    arguments += ' --timewarp ' + str(timewarp)
+    arguments += ' --shore_ip ' + shore_ip 
+    arguments += ' --boat_ip ' + boat_ip 
+    arguments += ' --boat_port ' + boat_port
+    print("Launching with Arguments: ", arguments)
+    entry_docker.exec_run('sh -c "python3.10 -u pyquaticus_moos_launcher.py '+arguments+' > out.txt 2>&1"', detach=True)# Run Entry
+    return 
+
+
+if __name__ == "__main__":
+    entry_folder = './entries/'
+    parser = argparse.ArgumentParser(description='Deploy the MCTF2025 Policies on USV\'s via MOOS-IvP')
+    parser.add_argument('--entry_name', required=True, type=str, help='Name of zip to be loaded in (do not add leading /)')
+    parser.add_argument('--sim', action='store_true', help="Specify if simulation or not.")
+    parser.add_argument('--color', required=True, choices=['red', 'blue'], help="Specify if red or blue team is the trained agent.")
+    parser.add_argument('--boat_id', required=True, choices=["blue_one", "blue_two", "blue_three", "red_one", "red_two", "red_three"], help="Specify the boat id.")
+    parser.add_argument('--boat_name', required=False, choices=['s', 't', 'u', 'v', 'w', 'x', 'y', 'z'], help="Specify the boat name.")
+    parser.add_argument('--timewarp', required=True, type=int, default=4, help='Specify the timewarp.')
+    parser.add_argument('--shore_ip', required=True, type=str, default='localhost', help='Specify the shoreside IP.')
+    parser.add_argument('--boat_ip', required=True, type=str, default='localhost', help='Specify the USV IP.')
+    parser.add_argument('--boat_port', required=True, type=int, default=9012, help='Specify the USV Port to Use.')
+    args = parser.parse_args()
+    entry_path = entry_folder + args.entry_name
+    signal.signal(signal.SIGINT, signal_handler)
+    run_game(entry_path, args.sim, args.color, args.boat_id, args.boat_name, args.timewarp, args.shore_ip, args.boat_ip, args.boat_port)
+    signal.pause()
